97394bcecdc75dc0e1d1362d8e13b7b9a3b87f4f
Mohsin Ali <meetsilverindigo@gmail.com>
2020-03-22 19:13:47 +0500

    Updates

---
+++ precache-manifest.549697c4167f9ead5b7374ab2f18b195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2098e2a3c55ecc1185370d58715913",
+    "url": "/bookingsteps/index.html"
+  },
+  {
+    "revision": "2575618e88adde5cc3ef",
+    "url": "/bookingsteps/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "71786d31500d0c17d4e3",
+    "url": "/bookingsteps/static/js/2.a736f1b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookingsteps/static/js/2.a736f1b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2575618e88adde5cc3ef",
+    "url": "/bookingsteps/static/js/main.55ed3be9.chunk.js"
+  },
+  {
+    "revision": "23c7d5e0cfac82a5adf1",
+    "url": "/bookingsteps/static/js/runtime-main.554f437a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bookingsteps/static/media/logo.5d5d9eef.svg"
+  }
+]);

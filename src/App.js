7a2e6bf99dd7e87b73969b7833a3b5f2f2e5f2cd
Mohsin Ali <meetsilverindigo@gmail.com>
2020-03-22 20:01:26 +0500

    Add bookintstepform component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import BookingStepForm from "./components/BookingStepForm/BookingStepForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <Switch>
+          <Route path="/" component={BookingStepForm} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
